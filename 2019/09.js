67cae5cedcdc0a7a1214f4e4e98aeaa78e91ff2f
h7c <h7c@dev.null>
2021-07-04 16:54:00 +0800

    update 2019/09.js, 2019/11.js, 2020/03.js, index.js

---
+++ 2019/09.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"29854450","published_at":"2019-09-10T09:45:25Z","title":"書房閒話：中秋將近蕭生賞析 為何蘇東坡的《水調歌頭》是宋詞第一 ?","tags":["中秋","中秋節","宋詞","水調歌頭","蕭若元","蘇東坡","蘇軾","詞","詩"],"youtube":"https://youtu.be/FwnhnyrB4is","url":"https://www.patreon.com/posts/shu-fang-xian-xi-29854450","image":"https://www.patreon.com/media-u/Z0FBQUFBQmc0WGNvV3Y4X1ptOVlVTlJVVnpKbDhRYVptVlF6bEpxV0NQUXpMaWR3VlBWaGlRMWJibUcwTTloWGNxTHpoVmprV1NMcnFDY0ZSYzJneVEza1JBMXdiXzBmQ3plVjFjQXFhMVBaRV9RYXZ0ODcwTXNQNkRkd0VXMUdwWW9EYlZFNk5aRWVlVnJVTjYyMmVwRHFqTmQ5dUMwOGN3PT0="},
+null
+])
