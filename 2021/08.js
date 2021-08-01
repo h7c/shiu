e350ff5042a8554f5d060088041b3893de99f2cb
h7c <h7c@dev.null>
2021-08-01 11:45:04 +0800

    update 2021/08.js, index.js

---
+++ 2021/08.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"54364742","published_at":"2021-08-01T03:45:01Z","title":"習大大自覺中國是戰無不勝  全因他擁有偉大光明的黨、愛護他的人民及英明領導人【中外碰碰車系列五】","tags":["2021","8月1日","August","中國","理論蕭析","美國","習近平","蕭若元"],"youtube":"https://youtu.be/pzcG1cIGDsY","url":"https://www.patreon.com/posts/xi-da-da-zi-jue-54364742","image":"https://www.patreon.com/media-u/Z0FBQUFBQmhCaGpBaDkyWUdGX3M0bGs3MnJZTVByWUxKaTBvRXRITURJQUhuRHl0S3BTWk8zUTlaUngyZUJyQW1mSG5RMU5BQTJWQkhpSDFDODFmMTBmbWM3U0VIVTVjVGJ1aFlrSV82ZEFtZFAwdXJMdUhUS2tFekUtWEphZVFfdlBuYWYyNmczV1JmeWc3VUIyQ3prU0FOWjd2dXdMdldRPT0="},
+null
+])
