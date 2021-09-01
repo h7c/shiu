99fa900f5c742ad75b5c10e26eeb59aedf5e4edc
h7c <h7c@dev.null>
2021-09-01 12:29:04 +0800

    update 2021/09.js, index.js

---
+++ 2021/09.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"55607512","published_at":"2021-09-01T04:28:40.355Z","title":"大陸已出紅頭文件提防恆大破產！講許家印如何由耕田變為世界負債第一！揭秘他和鄭裕彤 范冰冰 大劉甘比錯中複雜的關係！","tags":["2021","9月1日","September","中國","恆大","獨家","經濟","蕭若元"],"youtube":"https://youtu.be/AXtRsnOkXew","url":"https://www.patreon.com/posts/da-lu-yi-chu-tou-55607512","image":"https://www.patreon.com/media-u/Z0FBQUFBQmhMd0dRaGhJbG1nLVJ0bEM3b2RmQ0RJWXVhR3BMRlZzUnJrbkFKRy1FNjFrZS1iQjRxZXBmc0RRcF9xbVJTWl9jR2NrZk5UTkczaVZFbWZ5UUpsYjZyN3g0czNFM01LWG5pZDNXalV1RHJwUVNPWW5BOXdxeUVEVjZUOUJjSmNXWHAzeW5pQ0xWT3h0U0xFN2RONWVwT0N6blVBPT0="},
+null
+])
