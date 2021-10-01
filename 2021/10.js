1fdf9cf3a5981b6b563c2bfe4eb10afecdcd22f5
h7c <h7c@dev.null>
2021-10-01 11:00:04 +0800

    update 2021/10.js, index.js

---
+++ 2021/10.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"56835918","published_at":"2021-10-01T03:00:03Z","title":"恆大無力償還外國債息 被降級到破產邊緣！銀行地方政府為恆大隨時破產做準備  維穩機器全面啟動！廣州賣地情況淒慘 房地產泡沫何去何從？2021-10-01","tags":["10月1日","2021","October","中國","地產","恆大","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/SLpXAEEd5Hs","url":"https://www.patreon.com/posts/heng-da-wu-li-xi-56835918","image":"https://www.patreon.com/media-u/Z0FBQUFBQmhWbm0wd2pIZVFmWC15Z1hKbjRqamMxWUVtc3lMcmlyZ2N6dGJNUE12VGZTNGNUWEowZF9jblNYd1lKWjNjcUk4S0VBUlNzbGE0RHFiak1lZk4xdE9ONXRWUXU3NHA0SERlZm1kVWtfWkVnSlVkTkJ3SlYwRlhzMlpxWmZBQTJSS1RVTmJQZEZCcjk2a1Z5M05Kc01PUjFWbldnPT0="},
+null
+])
