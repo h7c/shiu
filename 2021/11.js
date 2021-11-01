2318acd9493730c134afbf582597a0de57700820
h7c <h7c@dev.null>
2021-11-01 12:00:05 +0800

    update 2021/11.js, index.js

---
+++ 2021/11.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"58135272","published_at":"2021-11-01T03:59:37.407Z","title":"上海網上可交易二手樓趕絕中介近百萬人因此失業！？中原地產即刻裁員7千人！富途和老虎幫大陸人買賣美股是犯法？各平台資訊出入嚴格管制遲早無得撈？大陸佛媛 病媛各種媛層出不窮！","tags":["11月1日","2021","November","中國","地產","經濟","蕭若元"],"youtube":"https://youtu.be/vgCBbmdSZCc","url":"https://www.patreon.com/posts/shang-hai-wang-58135272","image":"https://www.patreon.com/media-u/Z0FBQUFBQmhmMlpGM0daZ1RaZjEwVWFSdFhTSjNuMF80T09iY3daOEVZd3ZhSVVXejBBVDNYdXJmd1BtMjNaX2p2RFdmdnlhc0Yxbi1jSF9LdjRuUjVOR1RfS0NqcmJOZVdDdzA2VEJDTEV1NTlkQlhuYS0wRGtLT1cyYko5aFR2OEFWRTN5NVRQQk8tXzJ3T3hNT0dianRRRFZINTlIeFJRPT0="},
+null
+])
