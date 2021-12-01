cfaa506c97776005237f32bcb089bb242545a9d9
h7c <h7c@dev.null>
2021-12-01 14:30:04 +0800

    update 2021/12.js

--- 2021/12.js
+++ 2021/12.js
@@ -1,4 +1,5 @@
 __patreonPosts([
 {"id":"59358824","published_at":"2021-12-01T03:45:01Z","title":"拜習會休息一個月後 解放軍又派27戰機擾台！目的在抗議美國議員及立陶宛波羅的海三國議員屢次訪台？中國核潛艇浮上水面被發現了？阿森一族動畫嘲天安門太子站831一集在香港被抽走 2021-12-01","tags":["12月1日","2021","December","中國","台灣","蕭氏新聞台","蕭若元","香港"],"youtube":"https://youtu.be/M4t2J3L2ZT4","url":"https://www.patreon.com/posts/bai-xi-hui-xiu-59358824","image":"https://www.patreon.com/media-u/Z0FBQUFBQmhwdV9BTzRlMXd2VzdramdXcjQyelBWRnVFT1FkeFZPZm9hdC0xd2pGckFzVTlBdGNRZGRoeDZ6VkplSDNpd21WdFowOURsQjZJNVozcjFfTHVuN2ptNW1TUzRsaG5FemhSNkRReXRaSXJlNjBvUTNtVlZSSDZRdEJiMmFCWElRWUVjNmtIbVhUdzhIYWpIS1RYdl9oYllrcGtnPT0="},
+{"id":"59152046","published_at":"2021-12-01T06:30:01Z","title":"【史記重溫115】蕭若元細說《史記．孟嘗君列傳第十五》03： 孟嘗君在薛","tags":["2021","December","史記重溫","奇情歷史","蕭若元"],"youtube":"https://youtu.be/94aijpOsYC4","url":"https://www.patreon.com/posts/shi-ji-zhong-wen-59152046","image":"https://www.patreon.com/media-u/Z0FBQUFBQmhweFpzeWxrNk15SU5DS2RPUHVFak5sNTI0RmY5VnAwYkY5ZVRCdF9Fb2NuR1hRZlc3QjhyMjNPSWp2cU56M2RYVUdkNWlvb2VLSDdZamxvczJVaVNjc3JKM0tnaGRTa29kbFNrSi1wUW5RMlp0MEJlX0Vqb29lRHU3dTRyb21GZXo1clN1WVhaTjVsWFV4dHJvbGFoT3Z1LU5BPT0="},
 null
 ])
