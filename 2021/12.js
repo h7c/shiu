2b0e4bafcb4a8a54f905aab371c95433f52c9d69
h7c <h7c@dev.null>
2021-12-01 11:45:05 +0800

    update 2021/12.js, index.js

---
+++ 2021/12.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"59358824","published_at":"2021-12-01T03:45:01Z","title":"拜習會休息一個月後 解放軍又派27戰機擾台！目的在抗議美國議員及立陶宛波羅的海三國議員屢次訪台？中國核潛艇浮上水面被發現了？阿森一族動畫嘲天安門太子站831一集在香港被抽走 2021-12-01","tags":["12月1日","2021","December","中國","台灣","蕭氏新聞台","蕭若元","香港"],"youtube":"https://youtu.be/M4t2J3L2ZT4","url":"https://www.patreon.com/posts/bai-xi-hui-xiu-59358824","image":"https://www.patreon.com/media-u/Z0FBQUFBQmhwdV9BTzRlMXd2VzdramdXcjQyelBWRnVFT1FkeFZPZm9hdC0xd2pGckFzVTlBdGNRZGRoeDZ6VkplSDNpd21WdFowOURsQjZJNVozcjFfTHVuN2ptNW1TUzRsaG5FemhSNkRReXRaSXJlNjBvUTNtVlZSSDZRdEJiMmFCWElRWUVjNmtIbVhUdzhIYWpIS1RYdl9oYllrcGtnPT0="},
+null
+])
