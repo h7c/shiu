eafbd048166da6269bc518d86463589bd76c6368
h7c <h7c@dev.null>
2022-01-01 12:00:15 +0800

    update 2022/01.js, index.js

---
+++ 2022/01.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"60536569","published_at":"2022-01-01T04:00:02Z","title":"比較威廉二世與習近平如何建立人類命運共同體！意圖稱霸世界、建立新時代思想！而最終威廉二世是如何覆亡的？令德國經濟破產領土大減！〈蕭若元：理論蕭析〉2022-01-01","tags":["1月1日","2022","January","理論蕭析","蕭若元"],"youtube":"https://www.youtube.com/watch?v=iYxjPTk8ANU","url":"https://www.patreon.com/posts/bi-jiao-wei-lian-60536569","image":"https://www.patreon.com/media-u/Z0FBQUFBQmh6OUhQV1YtTVJoaktCanRXMkt6bTB2RzIzQlJxa1R4V3NnN3Q2c2d3NF9NTFdLMkVhUEZJd3NSckNZOHl0ek1STERQZ25OdzFHbnFBa2VVSGp6MkM4ZWJET0NQWVJ0Q180U0FhTG81cXJZbTVBa2trazlYX2w0SzJzaDV0RVdYTlN2QVJMUHU4cWU3VjdSNk5MemtURW1LanpRPT0="},
+null
+])
