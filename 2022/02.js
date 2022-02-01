f6bfb858fc07e25b5ac9067082c0832c9de505d7
h7c <h7c@dev.null>
2022-02-01 12:00:04 +0800

    update 2022/02.js

--- 2022/02.js
+++ 2022/02.js
@@ -1,4 +1,5 @@
 __patreonPosts([
 {"id":"61898750","published_at":"2022-02-01T02:00:03Z","title":"大話怕計數！英國及世衛報告話你知接種疫苗及加強劑後對應Omicron的差別！究竟染疫後再中的機會又有多大？","tags":[],"youtube":"https://youtu.be/UUYikXt8-as","url":"https://www.patreon.com/posts/da-hua-pa-ji-shu-61898750","image":"https://www.patreon.com/media-u/Z0FBQUFBQmgtSlFrWXNobENuVnZremhVdzlGbG5pOFh3WU5ZaDZYbGcyc0ZXUUFHcEZ4dFFaNGZvMnFqVEZ6djVWaEIwMUh2WXBLa0d2eXZ1V3duLVhaMEY0LTA2Y01qNER3akVWSFVXcVgtOVdvOUZiUFMtTmR1Z0FWa29La3VPU25ucFF6OFZ2MkVCNTEtNk1rQ21PbE1GenFqVkhzeFpBPT0="},
+{"id":"61887209","published_at":"2022-02-01T04:00:01Z","title":"你猜世上最大海參集散地在哪裡？估中無奬 正是香港！每年數百億海參出入口！教你如何揀靚海參！2022-02-01","tags":["2022","2月1日","February","書房閒話","蕭若元"],"youtube":"https://youtu.be/PGwhrQFKYlI","url":"https://www.patreon.com/posts/ni-cai-shi-shang-61887209","image":"https://www.patreon.com/media-u/Z0FBQUFBQmgtTEJFSXpVY1Q0b2d6eVRWMXNZem1tWng2U195UnRoN3c5M3I4anF3VWV5OExfLVh3UDFZQzRucWRSN1haVkRZcDQwVkplV05sWDcxUHpTNl9FZkNCNE5KeWgwN25qaU45N3FhQzU2MThVRzdfRjlDREw1dTNSNG1ZVkJ0eDdIOG5pSjBsbDNzR0ZXd2RhN0Zvb3VmY2FYR3hRPT0="},
 null
 ])
