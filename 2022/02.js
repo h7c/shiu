2f1651512433e1ca05b6924e4d164b8194c0ae09
h7c <h7c@dev.null>
2022-02-01 10:00:05 +0800

    update 2022/02.js, index.js

---
+++ 2022/02.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"61898750","published_at":"2022-02-01T02:00:03Z","title":"大話怕計數！英國及世衛報告話你知接種疫苗及加強劑後對應Omicron的差別！究竟染疫後再中的機會又有多大？","tags":[],"youtube":"https://youtu.be/UUYikXt8-as","url":"https://www.patreon.com/posts/da-hua-pa-ji-shu-61898750","image":"https://www.patreon.com/media-u/Z0FBQUFBQmgtSlFrWXNobENuVnZremhVdzlGbG5pOFh3WU5ZaDZYbGcyc0ZXUUFHcEZ4dFFaNGZvMnFqVEZ6djVWaEIwMUh2WXBLa0d2eXZ1V3duLVhaMEY0LTA2Y01qNER3akVWSFVXcVgtOVdvOUZiUFMtTmR1Z0FWa29La3VPU25ucFF6OFZ2MkVCNTEtNk1rQ21PbE1GenFqVkhzeFpBPT0="},
+null
+])
