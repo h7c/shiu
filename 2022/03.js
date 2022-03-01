c432d8f98571d8e7368580be1debe60231845616
h7c <h7c@dev.null>
2022-03-01 14:18:04 +0800

    update index.js, 2022/03.js

---
+++ 2022/03.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"63207005","published_at":"2022-03-01T06:17:07Z","title":"(獨家)美國終於將俄國踢出SWIFT！詳細分析對俄有多大影響？來看剔除SWIFT系統史上唯一例子：伊朗！《蕭若元：理論蕭析》2022-02-28","tags":["2022","3月1日","March","獨家","理論蕭析","蕭若元"],"youtube":"https://youtu.be/8hiq0TRtHUA","url":"https://www.patreon.com/posts/du-jia-mei-guo-e-63207005","image":"https://c10.patreonusercontent.com/4/patreon-media/p/post/63207005/025b709533fa436ebad52667a11afc27/eyJ3Ijo2MjB9/2.jpg?token-time=1647388800\u0026token-hash=506QLkIq4UZfafryqwGoE7YqrgbvSIERL90urPnXLN0%3D"},
+null
+])
