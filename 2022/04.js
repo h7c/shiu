280ae64b5bdc707e43130e077808c21fa73881f0
h7c <h7c@dev.null>
2022-04-01 02:01:06 +0000

    update 2022/04.js, index.js

---
+++ 2022/04.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"64554173","published_at":"2022-04-01T02:01:00Z","title":"俄國承諾不能盡信！？俄國部署基輔切爾尼戈夫撤軍？和談結果如何？Larry最新戰事報導！《Larry：軍武X蕭析》2022-03-25","tags":["2022","4月1日","Larry","Larry：軍武X蕭析","March","蕭若元"],"youtube":"https://youtu.be/8lSZy5o8nHs","url":"https://www.patreon.com/posts/e-guo-cheng-nuo-64554173","image":"https://www.patreon.com/media-u/Z0FBQUFBQmlSbHpoZHVXTU5DNUVEVW9IeHFPUEtFNFRkLUdfYmdhb1NFaGtSZFVuVS1jNWVKaUV0MldROUU3cXh1b3VDcnNpQW96ZlZvajAtbUhNVFp4eVVKSE9hSHJIbW5TcG5zRkNZdTRiVHJsZ1VMWEhKQkxjZlFQRC1qMGRFQmJRNXlhRFpmY0kxLUNBR0p0VC1MMVhoc0VBTV9WQlpnPT0="},
+null
+])
