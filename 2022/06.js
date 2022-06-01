11abe0755ec1c11678eb43761e2b7948dba62fbf
h7c <h7c@dev.null>
2022-06-01 06:31:11 +0000

    update 2022/06.js

--- 2022/06.js
+++ 2022/06.js
@@ -1,4 +1,5 @@
 __patreonPosts([
 {"id":"67168687","published_at":"2022-06-01T03:30:01Z","title":"為何Google營利下跌？受到極多困擾！告訴你Google麻煩還有好多！亞馬遜受電動車拖累今期營利再跌！Tesla盈利大增三倍 股票跌4成！因擔心大陸供應鏈！《蕭若元：蕭氏新聞台》2022-06-01","tags":["2022","6月1日","june","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/S0RtIMVuqnA","url":"https://www.patreon.com/posts/wei-he-li-xia-ji-67168687","image":"https://www.patreon.com/media-u/Z0FBQUFBQmlsdDFGbF9QTERyYkdjWlVZN2UybEVVNVNYR1RNa2xmY2xVMnBYTmhKQWJHcHNfTDBRMHpoNlF6OFVrejdXY2JEZ3liMjNDSjVURm1wX2hmQlMxbkJTUkt5Nmc0SGl1RWNvaURCTzV3eW9wSnNQMU5TVnQ4bldrWHUtNUVUS1hTYUxRWW5SR3hNUlViRHdocFdjdU1QT1ZRdVJRPT0="},
+{"id":"67116640","published_at":"2022-06-01T06:31:00Z","title":"【史記重溫263】蕭若元細說《史記．韓信盧綰列傳第三十三》01：韓王信者","tags":["2022","May","史記重溫","奇情歷史","蕭若元"],"youtube":"https://youtu.be/q9hK75B39Vw","url":"https://www.patreon.com/posts/shi-ji-zhong-wen-67116640","image":"https://www.patreon.com/media-u/Z0FBQUFBQmlsd2V2ZS11WU1JckZpZks5YWJhMDJLMXJWUWRtTzlXSW5haWNsM3I4MjJFNmgtMmpWaEQwMWk0VnBYaWxmbVN3aXdWX1dLZHNEMmh1bzVfR2kwZURuV2t1VWpvUEZZdDFSMXdHX09GUXNiU0dDSE1zN1dkeFVhN3M0M3hTWU51Wm51S2NnNXRmLWduMkJTTXJJbW1MTnZzeHh3PT0="},
 null
 ])
