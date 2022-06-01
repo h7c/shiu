143d036973319bbf5c7fad2509e5e2c1096b1693
h7c <h7c@dev.null>
2022-06-01 03:30:14 +0000

    update 2022/06.js, index.js

---
+++ 2022/06.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"67168687","published_at":"2022-06-01T03:30:01Z","title":"為何Google營利下跌？受到極多困擾！告訴你Google麻煩還有好多！亞馬遜受電動車拖累今期營利再跌！Tesla盈利大增三倍 股票跌4成！因擔心大陸供應鏈！《蕭若元：蕭氏新聞台》2022-06-01","tags":["2022","6月1日","june","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/S0RtIMVuqnA","url":"https://www.patreon.com/posts/wei-he-li-xia-ji-67168687","image":"https://www.patreon.com/media-u/Z0FBQUFBQmlsdDFGbF9QTERyYkdjWlVZN2UybEVVNVNYR1RNa2xmY2xVMnBYTmhKQWJHcHNfTDBRMHpoNlF6OFVrejdXY2JEZ3liMjNDSjVURm1wX2hmQlMxbkJTUkt5Nmc0SGl1RWNvaURCTzV3eW9wSnNQMU5TVnQ4bldrWHUtNUVUS1hTYUxRWW5SR3hNUlViRHdocFdjdU1QT1ZRdVJRPT0="},
+null
+])
