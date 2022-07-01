e3ac95163e1543219f90c526a328313413848697
h7c <h7c@dev.null>
2022-07-01 04:01:07 +0000

    update 2022/07.js, index.js

---
+++ 2022/07.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"68453556","published_at":"2022-07-01T04:00:07Z","title":"北約出最新戰略概念！指出中國欲系統性顛覆西方世界！運用假訊息挑撥離間 滲透軍事太空各大領域？！俄國更從「戰略夥伴」轉成「直接威脅」！《蕭若元：蕭氏新聞台》2022-06-30","tags":["2022","7月1日","July","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/thMxyfvC3_U","url":"https://www.patreon.com/posts/bei-yue-chu-zui-68453556","image":"https://www.patreon.com/media-u/Z0FBQUFBQml2bkdDMTF0ajZtaVg5Mml1cy1mUWlqVFdYeVJYbHQ0SXR4RXMzYllqby1WalZuU2RqSldESEZ6OFh4eGpocVp3X3I5dWFfU2dpTk5wSG5GSWdSQUNXREhQdzR0Slc5QXlEWjBjMkx1ZnRycF9aQy1tN1Uyb1V3NGRYT3hnS1VHVVg5dmZ0cG1PVEwtcDRmVkZQRmtuRm03Y2JBPT0="},
+null
+])
