0482770681be35a56f579dffba41516719faf7eb
h7c <h7c@dev.null>
2022-08-01 03:31:05 +0000

    update index.js, 2022/08.js

---
+++ 2022/08.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"69868040","published_at":"2022-08-01T03:31:00Z","title":"蠢人實錄！中國富二代是如何「坑爹」的？吉林水利廳長更因此被停職調查！在網上炫富之餘更公開父親全名？","tags":["2022","8月1日","July","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/LdzCp5VWHUs","url":"https://www.patreon.com/posts/chun-ren-shi-lu-69868040","image":"https://www.patreon.com/media-u/Z0FBQUFBQmk1MGo1dkJMZVpBc1M5eUMwdWlqZ0RndkpXdlUtdDBWbHlkdVotYUNMZDZ4cm9DMV9CamRTT3h5VUU2VlZVMHBCcDNTNFlxYnRjdkF5VzlPSXBtY09IZEZqZVhQVGZ1NmpTcTNWWHRjN1lTU19ZeE9yLTBUbmwxOUFFZWlOLVMtdDBEck51eC1Ma05MekJETEVlajJua280N1RBPT0=#162726543_620_None"},
+null
+])
