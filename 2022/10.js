92c9213b29b0e7522d3f31ca20597a14618fc40a
h7c <h7c@dev.null>
2022-10-01 03:30:07 +0000

    update 2022/10.js, index.js

---
+++ 2022/10.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"72675153","published_at":"2022-10-01T03:30:01Z","title":"猛招救樓市！連續三月下跌城市放寬貸款！深圳等地樓價半價求售！灰犀牛危機深不見底！《蕭若元：理論蕭析》2022-10-01","tags":["10月1日","2022","October","理論蕭析","蕭若元"],"youtube":"https://youtu.be/Rmm5E4F44I4","url":"https://www.patreon.com/posts/meng-zhao-jiu-xu-72675153","image":"https://www.patreon.com/media-u/Z0FBQUFBQmpON1FfRHZDNk91Nk95X3l3VjVmTnpNYlZUbmY4NGtJQV9lTUsxWnVnZ3l5SWw4RkYyWkNxVExzbDY3Z1k0QXZQdTY2SU15MHVOb2FZRXBRQVJtLTFYbVFDeEkxcTRBWFZJWjhSNWNFaGZ2dHkzUWxLcXd1ZF8zREwyMTN3OWFBWVNaUXRCT2h6ZGRIWWsyTjBOSmx2ajdjbDhnPT0=#170276264_620_None"},
+null
+])
