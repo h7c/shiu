653464b20a29691ec4c0b16571405ebab6226661
h7c <h7c@dev.null>
2022-11-01 01:30:06 +0000

    update 2022/11.js, index.js

---
+++ 2022/11.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"74053353","published_at":"2022-11-01T01:30:02Z","title":"馮小剛潤美！在美擁兩棟豪宅價值億萬美金！富豪紛紛出逃新加坡買樓！課重稅都要買！中國二三四線樓市大崩盤！一線城市勉強苦撐！《蕭若元：蕭氏新聞台》2022-11-01","tags":["11月1日","2022","November","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/mApB_a0CU8Q","url":"https://www.patreon.com/posts/feng-xiao-gang-74053353","image":"https://www.patreon.com/media-u/Z0FBQUFBQmpZSGFlWWN3anpPRm5IZV9ZbGF2dktWWXQtMWFvdmU2azJlV2RSUkpoN19JNld4OFBoZlB2ZDgzM2N2Z2x4dkFXUjhqWnc1b1hxeERoeTFWUE95aVFmX3Q4Y2FoNkl2QnFTeDJsTVd5NUFiWFdpcTNPeFh2S0lOb3pSM2toSlVtcWQzRkdlTmVlSjcyMm5YazQwQ2lSYk9VaktRPT0=#174397832_620_None"},
+null
+])
