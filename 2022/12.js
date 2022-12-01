f61577fb0539a864fb75e80901622d117b6a882d
h7c <h7c@dev.null>
2022-12-01 03:30:07 +0000

    update 2022/12.js, index.js

---
+++ 2022/12.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"75342160","published_at":"2022-12-01T03:30:01Z","title":"美國打擊中國科技發展下手有多重？深紫外光機台、零件設備服務、化學用品光阻劑全部加以限制！中低層技術記憶體都將被打到重傷！《蕭若元：蕭氏新聞台》2022-12-01","tags":["12月1日","2022","December","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/GBCbirGY8fA","url":"https://www.patreon.com/posts/mei-guo-da-ji-ke-75342160","image":"https://www.patreon.com/media-u/Z0FBQUFBQmppQi0tWUlqalVoWmh3QUI4Q1FHVWh0ZFRtdXF4dWU3OXVRRnNPYUktQ1ZoTVZVMzJReEZKRUVvZ2dOaWl2Nm1UeFVVUFhRSFdQdW5hNXk3RnFDYmZPa0FubHhQc05xd3F2RWVVZWF3aUpxUGpZMFpNQW05V184WElYSGcwMXRGYm1saUJ0Q3hEWThJV1RrNTN6SWdvem5IM1dRPT0=#178432496_620_None"},
+null
+])
