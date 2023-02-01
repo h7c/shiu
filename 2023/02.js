51520821b4a06b5a2d56908e841c7fb829044b5c
h7c <h7c@dev.null>
2023-02-01 03:32:11 +0000

    update 2023/02.js, index.js

---
+++ 2023/02.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"78009619","published_at":"2023-02-01T03:30:00Z","title":"三年封控完全是浪費時間！大陸人對習近平政策反應如何？女星春夏聲援白紙運動遭大陸官方封殺！更從央視春晚中除名！《蕭若元：蕭氏新聞台》2023-02-01","tags":["2023","2月1日","February","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/CO681gjZk7g","url":"https://www.patreon.com/posts/san-nian-feng-da-78009619","image":"https://c10.patreonusercontent.com/4/patreon-media/p/post/78009619/0040739256a14be5b24766a4d64a31fe/eyJ3Ijo2MjB9/3.jpg?token-time=1676505600\u0026token-hash=JfWWT9o9_UtYKNOmOV8bQjYTQ2xaMbtysEJsSWEnr7E%3D"},
+null
+])
