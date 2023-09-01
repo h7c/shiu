aca1de2010da8f613ad20f3a09f835a857e26428
h7c <h7c@dev.null>
2023-09-01 03:00:11 +0000

    update 2023/09.js

--- 2023/09.js
+++ 2023/09.js
@@ -1,4 +1,5 @@
 __patreonPosts([
 {"id":"88573363","published_at":"2023-09-01T00:30:01Z","title":"中國採訪自由不如30年前！ 資深美國記者：習對外媒極度不信任 二百斤「做壞事怕人知」對外媒限制遠超江胡、美商務部長指外資重投中國的前提 建立在中國是否言行一致 2023-09-01《香港新聞連線》報導","tags":[],"youtube":"https://youtu.be/1V7Y_teRbzU","url":"https://www.patreon.com/posts/zhong-guo-cai-zi-88573363","image":"https://c10.patreonusercontent.com/4/patreon-media/p/post/88573363/f8b62243df5646f18aa00bbb8ab720e4/eyJ3Ijo2MjB9/2.jpg?token-time=1694822400\u0026token-hash=2Ac97PFPPK4aQ3MRY_93399lv0uMVaAVlSOuLmh0BBQ%3D"},
+{"id":"88551400","published_at":"2023-09-01T03:00:02Z","title":"中國偷偷轉駄！為何熱門討論區仇日發言減少？我再駁斥中方關於核廢水的言論！在香港稱核廢水沒問題竟可以入罪？！《蕭若元：蕭氏新聞台》2023-09-01","tags":["2023","9月1日","September","蕭氏新聞台","蕭若元"],"youtube":"https://youtu.be/dtHGimiIn5U","url":"https://www.patreon.com/posts/zhong-guo-tou-he-88551400","image":"https://www.patreon.com/media-u/Z0FBQUFBQms4Vk82THJwNDZZbkJjbmNGam04LTJQc1JXSVBRMmVlS2VTNm5OdUk5Mk9Ha2FHVU00M2k1Q2ZjSkt4NGZoOFowaUJWYjVheUtDYjBLanVGbF9sWU5kaWpKLTVCTFBYQjhsdk4zQ21jdDhKZW5aREdWRTlWLXk0UTBUSENkWUd0d3hGeVlWOEppYnNob2lLWk9sdWpZTGhMWUl3PT0=#226301954_620_None"},
 null
 ])
