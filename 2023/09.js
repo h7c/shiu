34a1505c924daa7ef4ace93a1ff61c29db3be123
h7c <h7c@dev.null>
2023-09-01 00:30:09 +0000

    update 2023/09.js, index.js

---
+++ 2023/09.js
@@ -0,0 +1,4 @@
+__patreonPosts([
+{"id":"88573363","published_at":"2023-09-01T00:30:01Z","title":"中國採訪自由不如30年前！ 資深美國記者：習對外媒極度不信任 二百斤「做壞事怕人知」對外媒限制遠超江胡、美商務部長指外資重投中國的前提 建立在中國是否言行一致 2023-09-01《香港新聞連線》報導","tags":[],"youtube":"https://youtu.be/1V7Y_teRbzU","url":"https://www.patreon.com/posts/zhong-guo-cai-zi-88573363","image":"https://c10.patreonusercontent.com/4/patreon-media/p/post/88573363/f8b62243df5646f18aa00bbb8ab720e4/eyJ3Ijo2MjB9/2.jpg?token-time=1694822400\u0026token-hash=2Ac97PFPPK4aQ3MRY_93399lv0uMVaAVlSOuLmh0BBQ%3D"},
+null
+])
